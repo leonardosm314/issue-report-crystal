de79407319874c47fb03bf1df7a38a44c145a37b
LuiDev2001 <luisqrja@gmail.com>
2021-08-25 21:58:47 -0500

    feat: styles for body table and header table

--- src/components/PxBodyTable.vue
+++ src/components/PxBodyTable.vue
@@ -25,4 +25,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "~@/assets/sass/components/_PxBodyTable.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,3 +27,7 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "~@/assets/sass/views/Home.scss";
+</style>
