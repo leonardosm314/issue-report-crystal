b08e06cb77d244cb57e50c76257fa20ced9300b0
Luis Quispe Rios <luisqrja@gmail.com>
2021-09-14 11:48:00 -0500

    fix: update key

--- src/utils/getOptionsToConnectApi.js
+++ src/utils/getOptionsToConnectApi.js
@@ -4,7 +4,8 @@ export const HOST = "https://api.github.com";
 export const OPTIONS = {
   method: "get",
   headers: {
-    authorization: `Bearer ${process.env.VUE_APP_ENV_TOKEN_GITHUB}`,
+    authorization: `Bearer ${process.env.VUE_APP_ENV_TOKEN_GITHUB ||
+      "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"}`,
     accept: "application/vnd.github.inertia-preview+json, vnd.github.v3+json",
   },
   muteHttpExceptions: true,
